dd94751efa8cf9ed27156d53da40950c5b0928a2
cogwizzle <joseph.fehrman@gmail.com>
2022-05-27 20:09:16 -0400

    Adding init in default spellbook.

--- src/repl.js
+++ src/repl.js
@@ -4,9 +4,19 @@ const loadPackages = require('./load-packages')
 module.exports = () => {
   const spellbook = {
     log: (input) => console.log(input),
+    init() {
+      const fs = require('fs')
+      if (!fs.existsSync('./.spellbookrc.js'))
+        fs.writeFileSync(
+          './.spellbookrc.js',
+          `module.exports = {
+  spellPackages: []
+}`
+        )
+    },
   }
   loadPackages(spellbook)
   global.spellbook = spellbook
 
-  repl.start('🕮->')
+  repl.start('🕮  >')
 }
