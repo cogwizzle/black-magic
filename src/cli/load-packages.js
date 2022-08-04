00c078d27d93999a8f146379cc65646df3f41258
Joe Fehrman <joseph.fehrman@gmail.com>
2022-08-04 15:13:01 +0000

    Adding JSDoc to document the way the functions are intended to be used.

--- src/cli/load-packages.js
+++ src/cli/load-packages.js
@@ -1,5 +1,10 @@
 const { cosmiconfigSync } = require('cosmiconfig')
 
+/**
+ * Retrieves all modules from the .spellbookrc.js file and adds them into the spellbook.
+ * @param {Object} spellbook The spellbook, a collection of developer functions.
+ * @returns {void}
+ */
 module.exports = (spellbook) => {
   const explorer = cosmiconfigSync('spellbook')
   const spells = explorer.search()
