790f6e7bdeada70fc8148946eea5b803de314b82
Joe Fehrman <joseph.fehrman@gmail.com>
2022-08-04 01:08:19 +0000

    Using utility to create spells.

--- src/repl.js
+++ src/repl.js
@@ -1,33 +1,40 @@
 const repl = require('repl')
 const loadPackages = require('./load-packages')
 const loadSelector = require('./load-selector')
+const { createSpell } = require('./index')
 
 module.exports = () => {
-  const spellbook = {
-    log: (input) => console.log(input),
-    init() {
+  const spellbook = createSpell({
+    spellbook: createSpell({
+      spellbook: undefined,
+      spellPath: 'log',
+      spell: (input) => console.log(input),
+      help: () => {
+        console.group('Log ')
+        console.log('Description: Log output to the REPL')
+        console.log('Params:')
+        console.log('  - string value to output.')
+        console.groupEnd()
+      },
+    }),
+    spellPath: 'init',
+    spell: () => {
       const fs = require('fs')
       if (!fs.existsSync('./.spellbookrc.js'))
         fs.writeFileSync(
           './.spellbookrc.js',
           `module.exports = {
-  spellPackages: []
+spellPackages: []
 }`
         )
     },
-  }
-  spellbook.log.help = () => {
-    console.group('Log ')
-    console.log('Description: Log output to the REPL')
-    console.log('Params:')
-    console.log('  - string value to output.')
-    console.groupEnd()
-  }
-  spellbook.init.help = () => {
-    console.group('Init ')
-    console.log('Description: initialize black magic spell.')
-    console.groupEnd()
-  }
+    help: () => {
+      console.group('Init ')
+      console.log('Description: initialize black magic spell.')
+      console.groupEnd()
+    },
+  })
+
   loadPackages(spellbook)
   const spellbookName = loadSelector()
   global[spellbookName] = spellbook
